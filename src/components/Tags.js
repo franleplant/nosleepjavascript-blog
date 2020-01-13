f1d35a0e3f9df8891977e43c1974e4eeee030e3e
Fran Guijarro <franleplant@gmail.com>
2020-01-13 17:25:15 -0300

    format

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,6 +1,7 @@
 import React from "react"
+
 //TODO use emotion for css
-export function Tags({ tags }) {
+export default function Tags({ tags }) {
   return (
     <div
       style={{
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Tags from '../components/Tags'
+import Tags from "../components/Tags"
 import { rhythm, scale } from "../utils/typography"
 import getSetViewCount from "../dal/getSetPageView"
 
