55347a43487bc8b61fe4efb18f6fdf78b4460a68
Fran Guijarro <franleplant@gmail.com>
2020-10-25 19:16:53 -0300

    buy me coffe and patreon

---
+++ src/components/Patreon.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import useScript from "../hooks/useScript"
+
+export default function Patreon() {
+  useScript("https://c6.patreon.com/becomePatronButton.bundle.js")
+  return (
+    <a
+      href="https://www.patreon.com/bePatron?u=44509257"
+      data-patreon-widget-type="become-patron-button"
+    >
+      Become a Patron!
+    </a>
+  )
+}
